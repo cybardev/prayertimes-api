ddd1b1435a9833cfdc1ac232b7af846e0ee9e10e
Sheikh <sheikh@cybar.dev>
2023-11-14 09:44:47 -0400

    fix: correct handling of timezone offset and daylight savings

--- src/worker.ts
+++ src/worker.ts
@@ -1,14 +1,24 @@
-import prayTimes from "./PrayTimes.js";
+import prayTimes from "./PrayTimes.ts";
 
 function parseURL(url) {
     const args = new URL(url).searchParams;
     return {
+        latitude: parseFloat(args.get("latitude")),
+        longitude: parseFloat(args.get("longitude")),
         year: parseInt(args.get("year")),
         month: parseInt(args.get("month")),
         date: parseInt(args.get("date")),
-        latitude: parseFloat(args.get("latitude")),
-        longitude: parseFloat(args.get("longitude")),
+        tz_offset: parseInt(args.get("tz")),
+        dst: parseInt(args.get("dst")),
     };
+}
+
+function processPos(latitude, longitude) {
+    // default coordinates: Halifax, NS, Canada
+    const defaultPos = { latitude: 44.65, longitude: -63.57 };
+    return isNaN(latitude) || isNaN(longitude)
+        ? defaultPos
+        : { latitude: latitude, longitude: longitude };
 }
 
 function processDate(year, month, date) {
@@ -21,29 +31,37 @@ function processDate(year, month, date) {
     };
 }
 
-function processPos(latitude, longitude) {
-    // default coordinates: Halifax, NS, Canada
-    const defaultPos = { latitude: 44.65, longitude: -63.57 };
-    return isNaN(latitude) || isNaN(longitude)
-        ? defaultPos
-        : { latitude: latitude, longitude: longitude };
+function processTime(tz_offset, dst) {
+    // default time: Atlantic Standard Time (AST)
+    const ast = { tz_offset: -4, dst: 0 };
+    return {
+        tz_offset:
+            isNaN(tz_offset) || tz_offset < -12 || tz_offset > 12
+                ? ast.tz_offset
+                : tz_offset,
+        dst: [0, 1].includes(dst) ? dst : ast.dst,
+        timefmt: "12h",
+    };
 }
 
 function getPrayerTimes(params) {
     const date = processDate(params.year, params.month, params.date);
     const pos = processPos(params.latitude, params.longitude);
+    const timeinfo = processTime(params.tz_offset, params.dst);
     prayTimes.setMethod("ISNA");
     const times = prayTimes.getTimes(
         [date.year, date.month, date.date],
         [pos.latitude, pos.longitude],
-        "auto",
-        "auto",
-        "12h"
+        timeinfo.tz_offset,
+        timeinfo.dst,
+        timeinfo.timefmt
     );
     return {
         meta: {
             date: date,
             position: pos,
+            timezone_offset: timeinfo.tz_offset,
+            daylight_saving_time: timeinfo.dst,
         },
         data: {
             prayers: {
@@ -63,25 +81,22 @@ function getPrayerTimes(params) {
     };
 }
 
-export default {
-    async fetch(request, env, ctx) {
-        return request.method === "GET"
-            ? new Response(
-                  JSON.stringify(
-                      getPrayerTimes(parseURL(request.url)),
-                      undefined,
-                      2
-                  ),
-                  {
-                      status: 200,
-                      headers: {
-                          "content-type": "application/json;charset=UTF-8",
-                          "Access-Control-Allow-Methods": "GET",
-                          "Access-Control-Allow-Origin":
-                              "https://prayers.cybar.dev",
-                      },
-                  }
-              )
-            : new Response("Invalid Request: only GET requests supported");
-    },
+export default async (req, ctx) => {
+    return req.method === "GET"
+        ? new Response(
+              JSON.stringify(getPrayerTimes(parseURL(req.url)), undefined, 2),
+              {
+                  status: 200,
+                  headers: {
+                      "content-type": "application/json;charset=UTF-8",
+                      "Access-Control-Allow-Methods": "GET",
+                  },
+              }
+          )
+        : new Response("Invalid Request: only GET requests supported", {
+              status: 405,
+              headers: {
+                  "Access-Control-Allow-Methods": "GET",
+              },
+          });
 };
